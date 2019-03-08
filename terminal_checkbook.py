6ca086d1d52b84e9f032470993a20dca5f7098d3
Orion Wills <orion.wills@gmail.com>
2019-03-08 10:30:28 -0600

    base structures

---
+++ terminal_checkbook.py
@@ -0,0 +1,30 @@
+# This is the terminal_checkbook.py file
+
+# imports
+# time
+# pprint
+
+# Welcome function
+
+
+# Exit function
+
+
+# Input validation
+
+
+# Menu 1 prompt
+
+
+# Sub menu prompt
+
+
+# Menu items
+#     ~~~ Welcome to your terminal checkbook! ~~~
+
+#     What would you like to do?
+
+#     1) view current balance
+#     2) record a debit (withdraw)
+#     3) record a credit (deposit)
+#     4) exit
