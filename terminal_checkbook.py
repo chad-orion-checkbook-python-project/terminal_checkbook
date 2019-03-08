e17bbb66f2e677876d50b5e53aa3f8231271a42b
Chad_Hackney <hackney.chad@gmail.com>
2019-03-08 11:22:27 -0600

    ceh-initial log file field names

--- terminal_checkbook.py
+++ terminal_checkbook.py
@@ -32,4 +32,20 @@ while selection = input('What would you like to do?') in menu_options:
 #     3) record a credit (deposit)
 #     4) exit
 
+
+
 # Logging naming conventions
+
+# required fields
+trans_no #incrementing number, int
+trans #dollar value, float
+
+# optional add'l field
+running_total # incrementing number, float, populated in code
+date #now, date type, populated automatically on transaction
+time #now, time type, populated automatically on transaction
+trans_type # deposit or withdrawal, str, populated in code based on type of trans
+trans_desc #open text field for user input, str, populated via user input
+
+
+
